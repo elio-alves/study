c10dc480dcc75d0497fde0b7e9bfa3ed4bc37d17
Elio Alves Ribeiro Junior <devops@nava.com.br>
2022-01-03 15:14:36 -0300

    change folder

--- src/main/java/elio/alves/lambda/MathematicsExample.java
+++ src/main/java/elio/alves/lambda/MathematicsExample.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package elio.lambda;
+package elio.alves.lambda;
 
 import java.util.Arrays;
 import java.util.List;
